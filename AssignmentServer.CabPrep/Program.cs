d6d1776dc83366f3044f39a5bcd95d60115494f2
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2021-09-02 13:25:32 +0900

    dataprep toolchain

---
+++ AssignmentServer.CabPrep/Program.cs
@@ -0,0 +1,48 @@
+﻿using AssignmentServer.BlazorApp.Data;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AssignmentServer.CabPrep
+{
+    class Program
+    {
+        public static void PrepareStudentProto()
+        {
+            var path = "Cabinet/Students/proto.json";
+
+            if (!File.Exists(path))
+            {
+                return;
+            }
+
+            var json = File.ReadAllText(path);
+            var list = JsonConvert.DeserializeObject<IEnumerable<Student>>(json);
+
+            foreach (var student in list)
+            {
+                var cabDir = $"Cabinet/Students/{student.Id}";
+                var stdPath = $"{cabDir}/student.json";
+
+                if (File.Exists(stdPath))
+                    continue;
+                else if (Directory.Exists(cabDir))
+                    Directory.Delete(cabDir, true);
+
+                Directory.CreateDirectory(cabDir);
+                
+                var content = JsonConvert.SerializeObject(student);
+                File.WriteAllText(stdPath, content, Encoding.UTF8);
+
+                Console.WriteLine($"Wrote {student.Id}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PrepareStudentProto();
+        }
+    }
+}
