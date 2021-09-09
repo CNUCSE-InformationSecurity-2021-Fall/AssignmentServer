352663be8cb7cc9ba78e1dd002952d17d5744ab7
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2021-09-10 07:21:58 +0900

    project update

---
+++ AssignmentServer.ScoreCalc/Program.cs
@@ -0,0 +1,28 @@
+﻿using AssignmentServer.BlazorApp.Data;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace AssignmentServer.ScoreCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = "Cabinet/Students/";
+            var directories = Directory.GetDirectories(students);
+
+            foreach (var dir in directories)
+            {
+                var json = File.ReadAllText(dir + "/student.json");
+                var data = JsonConvert.DeserializeObject<Student>(json);
+
+                var assFiles = Directory.GetFiles(dir + "/Assignments");
+                foreach (var ass in assFiles)
+                {
+                    
+                }
+            }
+        }
+    }
+}
